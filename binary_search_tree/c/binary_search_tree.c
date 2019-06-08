30875186d9d13b11aec479aa947f4bbae9c09204
Mateusz Furga <matfurga@gmail.com>
2019-06-08 20:02:04 +0200

    Add binary search tree in C

---
+++ binary_search_tree/c/binary_search_tree.c
@@ -0,0 +1,169 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "binary_search_tree.h"
+
+#define max(a, b) ((a) > (b) ? (a) : (b))
+#define min(a, b) ((a) < (b) ? (a) : (b))
+
+binary_tree_node_t *create_binary_tree(int value)
+{
+  binary_tree_node_t *root = (binary_tree_node_t *)malloc(sizeof(binary_tree_node_t));
+  if (root == NULL) {
+    exit(EXIT_FAILURE);
+  }
+  root->value = value;
+  root->parent = NULL;
+  root->left = NULL;
+  root->right = NULL;
+  return root;
+}
+
+static binary_tree_node_t *add_left_child(binary_tree_node_t *node, int value)
+{
+  node->left = (binary_tree_node_t *)malloc(sizeof(binary_tree_node_t));
+  if (node->left == NULL) {
+    return NULL;
+  }
+  node->left->value = value;
+  node->left->parent = node;
+  node->left->left = NULL;
+  node->left->right = NULL;
+  return node->left;
+}
+
+static binary_tree_node_t *add_right_child(binary_tree_node_t *node, int value)
+{
+  node->right = (binary_tree_node_t *)malloc(sizeof(binary_tree_node_t));
+  if (node->right == NULL) {
+    return NULL;
+  }
+  node->right->value = value;
+  node->right->parent = node;
+  node->right->left = NULL;
+  node->right->right = NULL;
+  return node->right;
+}
+
+void insert(binary_tree_node_t *node, int value)
+{
+  binary_tree_node_t *curr = node;
+
+  while (curr->value > value && curr->left != NULL ||
+         curr->value < value && curr->right != NULL) {
+    if (curr->value > value)
+      curr = curr->left;
+    else
+      curr = curr->right;
+  }
+
+  if (curr->value > value)
+    add_left_child(curr, value);
+  else if (curr->value < value)
+    add_right_child(curr, value);
+  else return;  /* If they're the same, do nothing */
+}
+
+binary_tree_node_t *find(binary_tree_node_t *node, int value)
+{
+  while (node != NULL) {
+    if (node->value == value)
+      return node;
+
+    if (node->value > value)
+      node = node->left;
+    else if (node->value < value)
+      node = node->right;
+  }
+  return NULL;
+}
+
+void delete(binary_tree_node_t *node, int value)
+{
+  binary_tree_node_t *curr = find(node, value);
+  if (curr == NULL)
+    return;
+
+  /* The node doesn't have any children. */
+  if (curr->left == NULL && curr->right == NULL) {
+    if (curr->parent->left == curr)
+      curr->parent->left = NULL;
+    else
+      curr->parent->right = NULL;
+
+    free(curr);
+    return;
+  }
+
+  /* The node has only a left children. */
+  if (curr->right == NULL) {
+    curr->left->parent = curr->parent;
+
+    if (curr->parent->left == curr)
+      curr->parent->left = curr->left;
+    else
+      curr->parent->right = curr->left;
+
+    free(curr);
+    return;
+  }
+
+  /* The node has only a right children. */
+  if (curr->left == NULL) {
+    curr->right->parent = curr->parent;
+
+    if (curr->parent->left == curr)
+      curr->parent->left = curr->right;
+    else
+      curr->parent->right = curr->right;
+
+    free(curr);
+    return;
+  }
+
+  /* The node has two children. */
+  /* Now we're looking for the smallest value in the right sub-tree. */
+  binary_tree_node_t *smallest = curr->right;
+
+  while (smallest->left != NULL) {
+    smallest = smallest->left;
+  }
+
+  curr->value = smallest->value;
+  return delete(smallest, curr->value);
+}
+
+int height(binary_tree_node_t *node)
+{
+  if (node == NULL)
+    return 0;
+  return 1 + max(height(node->left), height(node->right));
+}
+
+int maximum(binary_tree_node_t *node)
+{
+  if (node == NULL)
+    return -1;
+
+  while (node->right != NULL)
+    node = node->right;
+  return node->value;
+}
+
+int minimum(binary_tree_node_t *node)
+{
+  if (node == NULL)
+    return -1;
+
+  while (node->left != NULL)
+    node = node->left;
+  return node->value;
+}
+
+int count(binary_tree_node_t *node)
+{
+  if (node == NULL)
+    return 0;
+  return 1 + count(node->left) + count(node->right);
+}
+
---
+++ binary_search_tree/c/binary_search_tree.h
@@ -0,0 +1,30 @@
+#ifndef BINARY_SEARCH_TREE_H
+#define BINARY_SEARCH_TREE_H
+
+typedef struct binary_tree_node
+{
+  int value;
+  struct binary_tree_node *parent;
+  struct binary_tree_node *left;
+  struct binary_tree_node *right;
+} binary_tree_node_t;
+
+binary_tree_node_t *create_binary_tree(int value);
+
+static binary_tree_node_t *add_left_child(binary_tree_node_t *node, int value);
+
+static binary_tree_node_t *add_right_child(binary_tree_node_t *node, int value);
+
+void insert(binary_tree_node_t *node, int value);
+
+binary_tree_node_t *find(binary_tree_node_t *node, int value);
+
+int height(binary_tree_node_t *node);
+
+int maximum(binary_tree_node_t *node);
+
+int minimum(binary_tree_node_t *node);
+
+int count(binary_tree_node_t *node);
+
+#endif  // BINARY_SEARCH_TREE_H
