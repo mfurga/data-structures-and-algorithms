5b3dae3ae8723555a718355279d1ee5860ed6f6b
Mateusz Furga <matfurga@gmail.com>
2019-06-10 12:05:39 +0200

    Add binary search tree in C++

---
+++ binary_search_tree/cc/binary_search_tree.cc
@@ -0,0 +1,197 @@
+#include <limits>
+#include "binary_search_tree.h"
+
+namespace mf
+{
+
+template <class T>
+BinarySearchTreeNode<T> *BinarySearchTreeNode<T>::add_left_child(const T& value)
+{
+  left_ = new BinarySearchTreeNode<T>(value, this);
+  return left_;
+}
+
+template <class T>
+BinarySearchTreeNode<T> *BinarySearchTreeNode<T>::add_right_child(const T& value)
+{
+  right_ = new BinarySearchTreeNode<T>(value, this);
+  return right_;
+}
+
+template <class T>
+void BinarySearchTree<T>::insert(const T& value)
+{
+  BinarySearchTreeNode<T> *curr = root_;
+
+  while (curr->value_ > value && curr->left_ != nullptr ||
+         curr->value_ < value && curr->right_ != nullptr) {
+    if (curr->value_ > value)
+      curr = curr->left_;
+    else
+      curr = curr->right_;
+  }
+
+  if (curr->value_ > value)
+    curr->add_left_child(value);
+  else if (curr->value_ < value)
+    curr->add_right_child(value);
+  else return;  /* If they're the same, do nothing */
+}
+
+template <class T>
+BinarySearchTreeNode<T> *BinarySearchTree<T>::find(BinarySearchTreeNode<T> *node,
+                                                   const T& value) const
+{
+  while (node != nullptr) {
+    if (node->value_ == value)
+      return node;
+    else if (node->value_ > value)
+      node = node->left_;
+    else
+      node = node->right_;
+  }
+  return nullptr;
+}
+
+template <class T>
+void BinarySearchTree<T>::remove(BinarySearchTreeNode<T> *node, const T& value)
+{
+  BinarySearchTreeNode<T> *curr = this->find(node, value);
+  if (curr == nullptr)
+    return;
+
+  /* The node doesn't have any children. */
+  if (curr->left_ == nullptr && curr->right_ == nullptr) {
+    if (curr->parent_->left_ == curr)
+      curr->parent_->left_ = nullptr;
+    else
+      curr->parent_->right_ = nullptr;
+
+    delete curr;
+    return;
+  }
+
+  /* The node has only a left children. */
+  if (curr->right_ == nullptr) {
+    curr->left_->parent_ = curr->parent_;
+
+    if (curr->parent_->left_ == curr)
+      curr->parent_->left_ = curr->left_;
+    else
+      curr->parent_->right_ = curr->left_;
+
+    delete curr;
+    return;
+  }
+
+  /* The node has only a right children. */
+  if (curr->left_ == nullptr) {
+    curr->right_->parent_ = curr->parent_;
+
+    if (curr->parent_->left_ == curr)
+      curr->parent_->left_ = curr->right_;
+    else
+      curr->parent_->right_ = curr->right_;
+
+    delete curr;
+    return;
+  }
+
+  /* The node has two children. */
+  /* Now we're looking for the smallest value in the right sub-tree. */
+  BinarySearchTreeNode<T> *smallest = curr->right_;
+
+  while (smallest->left_ != NULL) {
+    smallest = smallest->left_;
+  }
+
+  curr->value_ = smallest->value_;
+  return remove(smallest, smallest->value_);
+}
+
+template <class T>
+int BinarySearchTree<T>::height(BinarySearchTreeNode<T> *node)
+{
+  if (node == nullptr)
+    return -1;
+  return 1 + std::max(height(node->left_), height(node->right_));
+}
+
+template <class T>
+int BinarySearchTree<T>::count(BinarySearchTreeNode<T> *node)
+{
+  if (node == nullptr)
+    return 0;
+  return 1 + count(node->left_) + count(node->right_);
+}
+
+template <class T>
+BinarySearchTreeNode<T> *BinarySearchTree<T>::maximum() const
+{
+  BinarySearchTreeNode<T> *curr = root_;
+  while (curr->right_ != nullptr)
+    curr = curr->right_;
+  return curr;
+}
+
+template <class T>
+BinarySearchTreeNode<T> *BinarySearchTree<T>::minimum() const
+{
+  BinarySearchTreeNode<T> *curr = root_;
+  while (curr->left_ != nullptr)
+    curr = curr->left_;
+  return curr;
+}
+
+template <class T>
+bool BinarySearchTree<T>::is_between(BinarySearchTreeNode<T> *node,
+                                     int min, int max)
+{
+  if (node == nullptr)
+    return true;
+  return (node->value_ > min && node->value_ < max &&
+          is_between(node->left_, min, node->value_) &&
+          is_between(node->right_, node->value_, max));
+}
+
+/* 
+  We can also use the in-order traversal and check each time
+  that incoming data is sorted or not.
+ */
+template <class T>
+bool BinarySearchTree<T>::is_binary_search_tree()
+{
+  return is_between(root_, std::numeric_limits<int>::min(),
+                           std::numeric_limits<int>::max());
+}
+
+template <class T>
+BinarySearchTreeNode<T> *BinarySearchTree<T>::successor(const T& value) const
+{
+  BinarySearchTreeNode<T> *node = find(value);
+  if (node == nullptr)
+    return nullptr;
+
+  /* The node has a right sub-tree. */
+  if (node->right_ != nullptr) {
+    BinarySearchTreeNode<T> *curr = node->right_;
+    while (curr->left_ != nullptr)
+      curr = curr->left_;
+    return curr;
+  }
+
+  /* The node doesn't have a right sub-tree. */
+  BinarySearchTreeNode<T> *successor = nullptr;
+  BinarySearchTreeNode<T> *curr = root_;
+
+  while (curr != node) {
+    if (curr->value_ > node->value_) {
+      successor = curr;
+      curr = curr->left_;
+    } else
+      curr = curr->right_;
+  }
+  return successor;
+}
+
+}  // namespace mf
---
+++ binary_search_tree/cc/binary_search_tree.h
@@ -0,0 +1,60 @@
+#ifndef BINARY_SEARCH_TREE_H
+#define BINARY_SEARCH_TREE_H
+
+namespace mf
+{
+/* Used for the friend relationship between BinarySearchTree and BinarySearchTreeNode */
+template <class T> class BinarySearchTree;
+
+template <class T>
+class BinarySearchTreeNode
+{
+ private:
+  T value_;
+  BinarySearchTreeNode<T> *parent_;
+  BinarySearchTreeNode<T> *left_;
+  BinarySearchTreeNode<T> *right_;
+
+  friend class BinarySearchTree<T>;
+
+  public:
+  BinarySearchTreeNode(const T& value, BinarySearchTreeNode<T> *parent) : value_(value), 
+                       parent_(parent), left_(nullptr), right_(nullptr) {}
+  ~BinarySearchTreeNode() {}
+
+  BinarySearchTreeNode<T> *add_left_child(const T& value);
+  BinarySearchTreeNode<T> *add_right_child(const T& value);
+};
+
+template <class T>
+class BinarySearchTree
+{
+ private:
+  BinarySearchTreeNode<T> *root_;
+  int height(BinarySearchTreeNode<T> *node);
+  int count(BinarySearchTreeNode<T> *node);
+  void remove(BinarySearchTreeNode<T> *node, const T& value);
+  bool is_between(BinarySearchTreeNode<T> *node, int min, int max);
+  BinarySearchTreeNode<T> *find(BinarySearchTreeNode<T> *node, const T& value) const;
+
+ public:
+  BinarySearchTree(const T& value) { root_ = new BinarySearchTreeNode<T>(value, nullptr); }
+  ~BinarySearchTree() {}
+
+  void insert(const T& value);
+  void remove(const T& value) { return remove(root_, value); }
+  BinarySearchTreeNode<T> *find(const T& value) const { return find(root_, value); };
+
+  int height() { return height(root_); }
+  int count() { return count(root_); }
+
+  BinarySearchTreeNode<T> *maximum() const;
+  BinarySearchTreeNode<T> *minimum() const;
+
+  bool is_binary_search_tree();
+  BinarySearchTreeNode<T> *successor(const T& value) const;
+};
+
+}  // namespace mf
+
+#endif  // BINARY_SEARCH_TREE_H
