2a544506674cbf77d88cf7feb476eff8e69bb74c
Mateusz Furga <matfurga@gmail.com>
2019-06-19 20:32:33 +0200

    Add quick sort in C

---
+++ quick_sort/c/quick_sort.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#define swap(a, b) int tmp = (a); (a) = (b); (b) = tmp;
+
+static int partition(int *arr, int lo, int hi)
+{
+  /* We'll take the first element in the array as our pivot. */
+  int pivot = arr[lo];
+
+  int i = lo, j = hi + 1;
+  while (true) {
+    while (arr[++i] < pivot) if (i == hi) break;
+    while (arr[--j] > pivot); /* if (j == lo) break; */
+
+    if (j <= i) break;
+    swap(arr[i], arr[j]);
+  }
+
+  swap(arr[lo], arr[j]);
+  return j;
+}
+
+static void quicksort_(int *arr, int lo, int hi)
+{
+  if (hi <= lo) return;
+  int j = partition(arr, lo, hi);
+  quicksort(arr, lo, j - 1);
+  quicksort(arr, j + 1, hi);
+}
+
+void quicksort(int *arr, int size)
+{
+  return quicksort_(arr, 0, size - 1);
+}
---
+++ quick_sort/c/quick_sort.h
@@ -0,0 +1,10 @@
+#ifndef QUICK_SORT_H
+#define QUICK_SORT_H
+
+static int partition(int *arr, int lo, int hi);
+
+static void quicksort_(int *arr, int lo, int hi);
+
+void quicksort(int *arr, int size)
+
+#endif  // QUICK_SORT_H
