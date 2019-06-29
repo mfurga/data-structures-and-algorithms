704b571bcb29448ca2afb3a95c6f2e5b83fd8f47
Mateusz Furga <matfurga@gmail.com>
2019-06-29 13:15:55 +0200

    Update merge sort in Python

--- merge_sort/python/merge_sort.py
+++ merge_sort/python/merge_sort.py
@@ -1,30 +1,31 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
+from typing import List, Any
 
 
-class Mergesort(object):
-  def _merge(self, arr, copy, lo, mid, hi):
-    # Arr[lo] to arr[mid] and arr[mid + 1] to arr[hi] must be sorted.
-    copy[lo:hi + 1] = arr[lo:hi + 1]
+def _merge(arr: List[Any], copy: List[Any], lo: int, mid: int,
+           hi: int) -> None:
+  # Arr[lo] to arr[mid] and arr[mid + 1] to arr[hi] must be sorted.
+  copy[lo:hi + 1] = arr[lo:hi + 1]
 
-    i, j = lo, mid + 1
-    for k in xrange(lo, hi + 1):
-      if i > mid:
-        arr[k] = copy[j]; j += 1
-      elif j > hi:
-        arr[k] = copy[i]; i += 1
-      elif copy[i] < copy[j]:
-        arr[k] = copy[i]; i += 1
-      elif copy[i] >= copy[j]:
-        arr[k] = copy[j]; j += 1
+  i, j = lo, mid + 1
+  for k in range(lo, hi + 1):
+    if i > mid:
+      arr[k] = copy[j]; j += 1
+    elif j > hi:
+      arr[k] = copy[i]; i += 1
+    elif copy[i] < copy[j]:
+      arr[k] = copy[i]; i += 1
+    elif copy[i] >= copy[j]:
+      arr[k] = copy[j]; j += 1
 
-  def _sort(self, arr, copy, lo, hi):
-    if hi <= lo:
-      return
-    mid = lo + (hi - lo) / 2
-    self._sort(arr, copy, lo, mid)
-    self._sort(arr, copy, mid + 1, hi)
-    self._merge(arr, copy, lo, mid, hi)
+def _sort(arr: List[Any], copy: List[Any], lo: int, hi: int) -> None:
+  if hi <= lo:
+    return
+  mid = lo + (hi - lo) // 2
+  _sort(arr, copy, lo, mid)
+  _sort(arr, copy, mid + 1, hi)
+  _merge(arr, copy, lo, mid, hi)
 
-  def sort(self, arr):
-    copy = arr[:]
-    self._sort(arr, copy, 0, len(arr) - 1)
+def sort(arr: List[Any]) -> None:
+  copy = arr[:]
+  _sort(arr, copy, 0, len(arr) - 1)
