e69a498d1a10b67e22ca93bbb43229c0943c3f40
Mateusz Furga <matfurga@gmail.com>
2020-02-07 16:43:33 +0100

    Update insertion sort in C++

--- insertion_sort/cc/insertion_sort.cc
+++ insertion_sort/cc/insertion_sort.cc
@@ -1,14 +1,28 @@
 #include <iostream>
 
 template <typename T>
-void insertion_sort(T arr[], int size)
+void insertion_sort(T arr[], size_t size)
 {
   int j = 1;
-  for (int i = 1; i < size; i++) {
+  for (size_t i = 1; i < size; i++) {
     j = i;
-    while (j > 0 && arr[j] < arr[j - 1]) {
-      std::swap(arr[j], arr[j - 1]);
+    int key = arr[j];
+
+    while (j > 0 && arr[j - 1] < key) {
+      arr[j] = arr[j - 1];
       j--;
     }
+
+    arr[j] = key;
   }
+}
+
+int main()
+{
+  int arr[] = {5, 1, 3, -10, 2, 4};
+  size_t n = 6;
+
+  insertion_sort<int>(arr, n);
+
+  return 0;
 }
