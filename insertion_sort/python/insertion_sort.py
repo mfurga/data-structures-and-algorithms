e4378a2061e99503fc73b4c427ceca34cc31f047
Mateusz Furga <matfurga@gmail.com>
2020-02-07 16:28:26 +0100

    Update insertion sort in Python

--- insertion_sort/python/insertion_sort.py
+++ insertion_sort/python/insertion_sort.py
@@ -1,9 +1,18 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
+from typing import List
 
-def insertion_sort(arr):
-  for i in xrange(1, len(arr)):
+def insertion_sort(arr: List, size: int) -> None:
+  for i in range(1, size):
     j = i
-    while j > 0 and arr[j] < arr[j - 1]:
-      arr[j], arr[j - 1] = arr[j - 1], arr[j]
+    key = arr[i]
+
+    while j > 0 and arr[j - 1] > key:
+      arr[j] = arr[j - 1]
       j -= 1
+
+    arr[j] = key
+
+if __name__ == "__main__":
+  arr = [6, 3, 5, 10, -1, 7, -4]
+  insertion_sort(arr, len(arr))
