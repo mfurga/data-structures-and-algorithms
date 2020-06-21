ee0d7e7a5454119b5a6942a8799342a0ac5d0239
Mateusz Furga <matfurga@gmail.com>
2020-06-21 15:36:46 +0200

    Update array in Java

--- arrays/java/DynamicArray.java
+++ arrays/java/DynamicArray.java
@@ -1,26 +1,22 @@
 
-public class DynamicArray
-{
+public class DynamicArray {
   private int capacity;
   private int size;
   private Object[] array;
 
-  public DynamicArray()
-  {
+  public DynamicArray() {
     this.capacity = 16;
     this.size = 0;
     this.array = new Object[this.capacity];
   }
 
-  public DynamicArray(int capacity)
-  {
+  public DynamicArray(int capacity) {
     this.capacity = capacity;
     this.size = 0;
     this.array = new Object[this.capacity];
   }
 
-  private void resize()
-  {
+  private void resize() {
     int newCapacity = this.capacity * 2;
     Object[] newArray = new Object[newCapacity];
     System.arraycopy(this.array, 0, newArray, 0, this.size);
@@ -29,40 +25,35 @@ public class DynamicArray
     this.capacity = newCapacity;
   }
 
-  public void pushBack(Object element)
-  {
+  public void pushBack(Object element) {
     if (this.size >= this.capacity) {
       this.resize();
     }
     this.array[this.size++] = element;
   }
 
-  public Object popBack() throws Exception
-  {
+  public Object popBack() throws Exception {
     if (this.size <= 0) {
       throw new Exception("DynamicArray is empty");
     }
     return this.array[--this.size];
   }
 
-  public Object get(int index) throws Exception
-  {
+  public Object get(int index) throws Exception {
     if (index < 0 || index >= this.size) {
       throw new Exception("Index out of range");
     }
     return this.array[index];
   }
 
-  public void set(Object element, int index) throws Exception
-  {
+  public void set(Object element, int index) throws Exception {
     if (index < 0 || index >= this.size) {
       throw new Exception("Index out of range");
     }
     this.array[index] = element;
   }
 
-  public void insert(Object element, int index) throws Exception
-  {
+  public void insert(Object element, int index) throws Exception {
     if (index < 0 || index >= this.size) {
       throw new Exception("Index out of range");
     }
@@ -79,8 +70,7 @@ public class DynamicArray
     this.size++;  
   }
 
-  public void delete(int index) throws Exception
-  {
+  public void delete(int index) throws Exception {
     if (index < 0 || index >= this.size) {
       throw new Exception("Index out of range");
     }
@@ -90,8 +80,7 @@ public class DynamicArray
     this.size--;
   }
 
-  public int find(Object element) throws Exception
-  {
+  public int find(Object element) throws Exception {
     for (int i = 0; i < this.size; i++) {
       if (this.array[i] == element) {
         return i;
