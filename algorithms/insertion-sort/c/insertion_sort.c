0bebd55cc3448d510b358ef75dccc905b22fd6a3
Mateusz Furga <matfurga@gmail.com>
2021-02-25 10:30:43 +0100

    Update insertion sort in C

--- algorithms/insertion-sort/c/insertion_sort.c
+++ algorithms/insertion-sort/c/insertion_sort.c
@@ -1,29 +1,26 @@
 #include <stdio.h>
 
-void insertion_sort(int *arr, size_t size)
+void insertion_sort(int *arr, int sz)
 {
-  size_t i = 1, j = 1;
-  int key;
+  int i, j, key;
 
-  for (i = 1; i < size; i++) {
-    j = i;
-    key = arr[j];
+  for (i = 1; i < sz; i++) {
+    key = arr[i];
+    j = i - 1;
 
-    while (j > 0 && arr[j - 1] < key) {
-      arr[j] = arr[j - 1];
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
       j--;
     }
 
-    arr[j] = key;
+    arr[j + 1] = key;
   }
 }
 
 int main(void)
 {
-  int arr[] = {5, 1, 3, -10, 2, 4};
-  size_t n = 6;
-
-  insertion_sort(arr, n);
-
+  int arr[] = {5, 2, 4, 6, 1, 3};
+  insertion_sort(arr, 6);
   return 0;
 }
+
