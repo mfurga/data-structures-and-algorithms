22f25065448c4b5637db4988b73900958945f909
Mateusz Furga <matfurga@gmail.com>
2021-03-12 11:32:47 +0100

    Update quicksort in Python

--- algorithms/quick-sort/python/quicksort.py
+++ algorithms/quick-sort/python/quicksort.py
@@ -1,29 +1,23 @@
 #!/usr/bin/env python3
 
-def _partition(arr, lo, hi):
-  pivot = arr[lo]
-  i, j = lo + 1, hi
+def _partition(a, lo, hi):
+  p = a[hi]
+  i = lo - 1
 
-  while True:
-    while arr[i] < pivot:
-      if i == hi: break
+  for j in range(lo, hi):
+    if a[j] < p:
       i += 1
-    while arr[j] > pivot:
-      j -= 1
+      a[i], a[j] = a[j], a[i]
 
-    if j <= i: break
-    arr[i], arr[j] = arr[j], arr[i]
+  a[i + 1], a[hi] = a[hi], a[i + 1]
+  return i + 1
 
-  arr[lo], arr[j] = arr[j], arr[lo]
-  return j
+def _quicksort(a, lo, hi):
+  if lo < hi:
+    j = _partition(a, lo, hi)
+    _quicksort(a, lo, j - 1)
+    _quicksort(a, j + 1, hi)
 
-def _quicksort(arr, lo, hi):
-  if lo >= hi:
-    return
-  j = _partition(arr, lo, hi)
-  _quicksort(arr, lo, j - 1)
-  _quicksort(arr, j + 1, hi)
-
-def quicksort(arr):
-  return _quicksort(arr, 0, len(arr) - 1)
+def quicksort(A):
+  return _quicksort(A, 0, len(A) - 1)
 
